de50b03c9f005eafad2b85ff9bb56bbb236d356f
Umairgujjar96 <umairhassn83937@gmail.com>
2025-07-07 19:05:56 +0500

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,7 +10,7 @@ import {Route, Routes} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import Product from './pages/Products';
 
-
+console.log("HI")
 
 function App() {
   return (
